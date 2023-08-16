096809e7ffe5b8b64c9530a447c66a9cddc3da4d
malte Liam Jakob <malteliam.jakob@rwu.de>
2023-08-16 20:52:20 +0200

    Add private browsing warning

--- src/views/settings/security.vue
+++ src/views/settings/security.vue
@@ -5,7 +5,7 @@
     <h1 class="text-h2">Security</h1>
     <p>This Application may store sensitive data about your medication intake. Thus, security is of utmost importance to us.</p>
     <p>Which pills you have taken is only stored as long as it is necessary to track a potential overdose. These time limits are checked at startup or can be refreshed manually.</p>
-    <p>All your Data is stored locally and can only be accessed by accessing your device.</p>
+    <p>All your Data is stored locally and can only be accessed by accessing your device. <b>If you are in private browsing mode, your data will be erased after you close the browser</b></p>
     <p>Additionally, you can protect your information using a password. It will e encrypted before saving it to disk and we only store it in your browser while you use the app (session storage). If you forget your password, your information cannot be recovered.</p>
   </v-container>
 </template>
