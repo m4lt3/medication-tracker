dfe557177a3186fcc9c8f75e5e72f1d5bd543be4
malte Liam Jakob <malteliam.jakob@rwu.de>
2023-08-16 21:27:14 +0200

    Implement Pill Group CRUD

---
+++ src/components/pills/groupForm.vue
@@ -0,0 +1,96 @@
+<script setup>
+import { ref, watch } from 'vue';
+import { useIndexedStore } from '@/store/indexed';
+
+import DeleteModal from '@/components/deleteModal.vue';
+
+const indexedStore = useIndexedStore();
+
+const props = defineProps([ 'edit' ]);
+const emit = defineEmits([ 'change' ]);
+
+const feedback = ref({ visible: false });
+const expanded = ref(null);
+const group = ref({});
+
+const rules = {
+  required: (value) => {
+    if (value) {
+      return true;
+    }
+    return "Required"
+  }
+};
+const form = ref(null);
+const valid = ref(false);
+
+
+watch(props, (newVal, oldVal) => {
+  if (props.edit !== null) {
+    group.value = JSON.parse(JSON.stringify(indexedStore.pillGroups[props.edit]));
+    group.value.id = props.edit;
+    expanded.value = 0;
+  } else {
+    group.value = {};
+  }
+})
+
+async function save() {
+  await form.value.validate();
+  if (!valid.value) {
+    return;
+  }
+  try {
+    let entry = JSON.parse(JSON.stringify(group.value));
+    if (entry.id) {
+      let id = Number(entry.id);
+      delete entry.id;
+      await indexedStore.update("pillGroups", id, entry);
+      feedback.value = { visible: true, type: 'success', title: 'Changes saved!', text: 'Your changes have been saved' };
+    } else {
+      await indexedStore.add("pillGroups", entry);
+      feedback.value = { visible: true, type: 'success', title: 'Group added!', text: 'You can now add Pills to this group' };
+    }
+    group.value = {};
+  } catch (e) {
+    feedback.value = { visible: true, type: 'error', title: 'Something went wrong!', text: e };
+  }
+}
+
+async function deleteGroup(id) {
+  try {
+    await indexedStore.remove("pillGroups", id);
+    feedback.value = { visible: true, type: 'success', title: 'Group deleted!', text: '' };
+    emit('change');
+  } catch (e) {
+    feedback.value = { visible: true, type: 'error', title: 'Something went wrong!', text: e };
+  }
+}
+
+</script>
+<template>
+  <v-expansion-panels v-model="expanded">
+    <v-expansion-panel>
+      <v-expansion-panel-title>
+        Add or Edit Group
+      </v-expansion-panel-title>
+      <v-expansion-panel-text>
+        <v-form ref="form" v-model="valid">
+          <v-text-field v-model="group.name" label="name" :rules="[rules.required]"></v-text-field>
+          <div class="d-flex">
+            <v-btn color="blue" prepend-icon="mdi-content-save" @click="save" style="flex-grow: 3" >Save</v-btn>
+            <DeleteModal v-if="group.id" style="flex-grow: 1; margin-left: 4px;" @delete="deleteGroup(group.id)"></DeleteModal>
+          </div>
+        </v-form>
+
+      </v-expansion-panel-text>
+    </v-expansion-panel>
+  </v-expansion-panels>
+  <v-alert
+    v-model="feedback.visible"
+    v-bind="feedback"
+    closable
+    class="my-2"
+  >
+  </v-alert>
+</template>
--- src/views/settings/pills.vue
+++ src/views/settings/pills.vue
@@ -1,4 +1,12 @@
 <script setup>
+import { ref } from 'vue'
+import { useIndexedStore } from '@/store/indexed';
+
+import GroupForm from '@/components/pills/groupForm.vue';
+
+const indexedStore = useIndexedStore();
+const editId = ref(null);
+
 </script>
 <template>
   <v-container>
@@ -7,6 +15,21 @@
     <p>You can use Pill groups for a better overview in your dorpdown. Other than that, they don't have much use.</p>
     <p>Pills can belong to a Group and can contain one or multiple of your tracked ingredients and on selecting, you can also decite between a whole or just half a pill.</p>
     <h2 class="text-h3">Pill Groups</h2>
+    <GroupForm :edit="editId" @change="editId = null"></GroupForm>
+    <v-table>
+      <thead>
+        <tr>
+          <th>Name</th>
+          <th></th>
+        </tr>
+      </thead>
+      <tbody>
+        <tr v-for="(group, id) in indexedStore.pillGroups" :key="id">
+          <td>{{ group.name }}</td>
+          <td><v-btn color="blue" variant="plain" icon="mdi-square-edit-outline" @click="editId = id"></v-btn></td>
+        </tr>
+      </tbody>
+    </v-table>
     <h2 class="text-h3">Pills</h2>
   </v-container>
 </template>
