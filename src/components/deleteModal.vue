adb1386a83595cbc8e05fd4b2753a7847945a7af
malte Liam Jakob <malteliam.jakob@rwu.de>
2023-08-16 20:04:12 +0200

    Implement CRUD of Ingredients

---
+++ src/components/deleteModal.vue
@@ -0,0 +1,33 @@
+<script setup>
+import { ref } from 'vue';
+
+const properties = defineProps([ 'style' ]);
+const emit = defineEmits(['delete']);
+
+const show = ref(false);
+</script>
+<template>
+  <v-dialog
+    width="auto"
+    v-model="show"
+  >
+    <template #activator="{ props }">
+      <v-btn color="error" v-bind="props" :style="properties.style" ><v-icon icon="mdi-delete-outline"></v-icon></v-btn>
+    </template>
+    <v-card
+      width="auto"
+    >
+      <v-card-item>
+        <v-card-title>Are you sure?</v-card-title>
+      </v-card-item>
+      <v-card-text>
+        <p>You won't be able to recover deleted data</p>
+        <p>Items referencing this entry must be deleted or altered first.</p>
+      </v-card-text>
+      <v-card-actions>
+        <v-btn color="error" @click="$emit('delete'); show = false">Yes, delete</v-btn>
+        <v-btn @click="show = false" color="secondary">Never mind</v-btn>
+      </v-card-actions>
+    </v-card>
+  </v-dialog>
+</template>
--- src/views/settings/ingredients.vue
+++ src/views/settings/ingredients.vue
@@ -1,5 +1,200 @@
 <script setup>
+import { ref, watch } from 'vue';
+import { useIndexedStore } from '@/store/indexed';
+
+import DeleteModal from '@/components/deleteModal.vue';
+
+const indexedStore = useIndexedStore();
+
+const feedback = ref({ visible: false});
+
+const panelExpanded = ref();
+const category = ref({ expiresUnit: 'minute', expiresAmount: 0 });
+const categoryForm = ref(null);
+const formValid = ref(false);
+
+const bufferChanged = ref(false);
+const amount = ref(0);
+const unit = ref("minute");
+const ms = {
+  minute: 60000,
+  hour: 3600000,
+  day: 86400000
+}
+
+async function save() {
+  await categoryForm.value.validate();
+  if (!formValid) {
+    return;
+  }
+
+  let entry = JSON.parse(JSON.stringify(category.value));
+
+  entry.limit = Number(entry.limit);
+  entry.expires = convertUnitsToMs(entry);
+  delete entry.expiresAmount;
+  delete entry.expiresUnit;
+
+
+  if (category.value.id) {
+    let id = Number(entry.id);
+    delete entry.id;
+    indexedStore.update("categories", id, entry);
+    feedback.value = { visible: true, type: 'success', title: 'Schanges saved', message: 'Your changes have been saved'};
+  } else {
+    indexedStore.add("categories", entry);
+    feedback.value = { visible: true, type: 'success', title: 'Ingredient added', message: 'The ingredient has been addet for tracking. You may reference it in pills now'};
+  }
+  category.value = {};
+}
+
+function edit(pCategory, id) {
+  category.value = JSON.parse(JSON.stringify(pCategory));
+  category.value.id = id;
+  panelExpanded.value = 0;
+}
+
+function convertMsToUnits(entry) {
+  let unit, amount;
+  let testUnits = ["minute", "hour", "day"];
+  let unitIndex = 0;
+
+  //detecting what is an appropriate unit size
+  for (let index in testUnits ) {
+    if (ms[testUnits[index]] > entry.expires) {
+      unitIndex = Math.max(0, index - 1);
+      break;
+    }
+  }
+
+  unit = testUnits[unitIndex];
+
+  // detecting amount of that unit
+  let tempAmount = 0;
+  let buffer = Number(entry.expires.toString())
+  while (buffer > ms[testUnits[unitIndex]]) {
+    tempAmount++;
+    buffer -= ms[testUnits[unitIndex]];
+  }
+  if (buffer > 0) {
+    tempAmount += buffer / ms[testUnits[unitIndex]];
+  }
+  amount = tempAmount;
+
+  return [ amount, unit ];
+}
+
+function convertUnitsToMs() {
+  return ms[category.value.expiresUnit] * category.value.expiresAmount;
+}
+
+function generateTimeText(entry) {
+  let [amount, unit] = convertMsToUnits(entry);
+  let s = (amount != 1 ? 's' : '');
+
+  return `${amount} ${unit}${s}`;
+}
+
+watch(category, (newVal, oldVal) => {
+  if (category.value.expires !== undefined) {
+    let [amount, unit] = convertMsToUnits(category.value);
+
+    category.value.expiresAmount = amount;
+    category.value.expiresUnit = unit;
+  }
+});
+
+function deleteCategory(id) {
+  try {
+    indexedStore.remove("categories", id);
+    feedback.value = { visible: true, type: 'success', title: 'Success!', message: 'Ingredient deleted' };
+    category.value = {};
+  } catch (e) {
+    feedback.value = { visible: true, type: 'error', title: 'Something went wrong', message: e};
+  }
+}
 </script>
 <template>
-  Ingredients
+  <h1 class="text-h2">Ingredients</h1>
+  <p>
+    Ingredients are the main purpose of this Application. Here you can enter what trackable ingredients your pills can contain, as well as your personal limit and how long this limit should be kept.
+  </p>
+  <p>
+    For example, a person with a body weight of 75kg should not consume more than 800mg of Ibuprofen within 6 hours:
+  </p>
+  <v-list>
+    <v-list-item
+      v-for="(item, index) in [{ subtitle: 'Name', title: 'Ibuprofen' }, { subtitle: 'Limit', title: '800' }, { subtitle: 'In my body', title: '6 Hours' }]"
+      :key="index"
+      :title="item.title"
+      :subtitle="item.subtitle"
+    >
+    </v-list-item>
+  </v-list>
+  <p>
+    Remember that you should always talk about these Values with a professional, als personal limits and time frames can vary based on may factors.
+  </p>
+  <v-expansion-panels class="mt-2" v-model="panelExpanded">
+    <v-expansion-panel>
+      <v-expansion-panel-title>
+        Edit or Add Ingredients
+      </v-expansion-panel-title>
+      <v-expansion-panel-text>
+        <v-form ref="categoryForm" v-model="formValid">
+          <v-text-field
+            label="Name"
+            v-model="category.name"
+          ></v-text-field>
+          <v-text-field
+            label="Limit (mg)"
+            v-model="category.limit"
+            type="number"
+            min="0"
+          ></v-text-field>
+          <v-row>
+            <v-col cols="6">
+              <v-text-field
+                label="In my body for"
+                v-model="category.expiresAmount"
+                type="number"
+                min="0"
+              ></v-text-field>
+            </v-col>
+            <v-col cols="6">
+              <v-select
+                :items="[{ title:'Minute(s)', value: 'minute' }, { title:'Hour(s)', value: 'hour' }, { title:'Day(s)', value: 'day' }]"
+                v-model="category.expiresUnit"
+                class="mx-1"
+              ></v-select>
+            </v-col>
+          </v-row>
+          <div class="d-flex flex-row flex-wrap">
+            <v-btn @click="save" color="blue" prepend-icon="mdi-content-save" style="flex-grow: 3">Save</v-btn>
+            <DeleteModal v-if="category.id" style="flex-grow: 1; margin-left: 4px;" @delete="deleteCategory(category.id)"></DeleteModal>
+          </div>
+        </v-form>
+      </v-expansion-panel-text>
+    </v-expansion-panel>
+  </v-expansion-panels>
+  <v-alert v-model="feedback.visible" :type="feedback.type" :title="feedback.title" :text="feedback.message" class="my-2" closable></v-alert>
+  <v-table
+    hover
+  >
+    <thead>
+      <tr>
+        <th>Name</th>
+        <th>Limit</th>
+        <th>Period</th>
+        <th></th>
+      </tr>
+    </thead>
+    <tbody>
+      <tr v-for="(category, id) in  indexedStore.categories" :key="id">
+        <td>{{ category.name }}</td>
+        <td>{{ category.limit }} mg</td>
+        <td>{{ generateTimeText(category) }}</td>
+        <td><v-btn color="blue" variant="plain" icon="mdi-square-edit-outline" @click="edit(category, id)"></v-btn></td>
+      </tr>
+    </tbody>
+  </v-table>
 </template>
