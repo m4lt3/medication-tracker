ade87840b6dfda1d42709b980d7e5c1ea78e9db9
malte Liam Jakob <malteliam.jakob@rwu.de>
2023-08-17 18:21:55 +0200

    Actually display components

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,9 +1,22 @@
+<script setup>
+import { useIndexedStore } from '@/store/indexed';
+
+import LimitBar from '@/components/home/limitBar.vue';
+import IntakeForm from '@/components/home/intakeForm.vue';
+
+const indexedStore = useIndexedStore();
+</script>
 <template>
   <v-container>
-    Home
+    <h1 class="text-h2">Current values</h1>
+    <LimitBar
+      v-for="(ingredient, id) in indexedStore.categories"
+      :key="id"
+      :ingredientInfo="ingredient"
+      currentValue="700"
+      :changes="{ amount:50, in: '13 minutes' }"
+    ></LimitBar>
+    <h1 class="text-h2">Intakes</h1>
+    <IntakeForm></IntakeForm>
   </v-container>
 </template>
-
-<script setup>
-
-</script>
