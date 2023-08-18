c3d19b9c469320edbd965ee53b089f59bac1d7db
malte Liam Jakob <malteliam.jakob@rwu.de>
2023-08-18 09:22:27 +0200

    Add more space in between bars

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -10,13 +10,19 @@ const indexedStore = useIndexedStore();
 <template>
   <v-container>
     <h1 class="text-h2">Current values</h1>
-    <LimitBar
-      v-for="(ingredient, id) in indexedStore.categories"
-      :key="id"
-      :ingredientInfo="ingredient"
-      currentValue="700"
-      :changes="{ amount:50, in: '13 minutes' }"
-    ></LimitBar>
+    <v-row
+    v-for="(ingredient, id) in indexedStore.categories"
+    :key="id"
+    >
+      <v-col cols="12">
+        <LimitBar
+
+          :ingredientInfo="ingredient"
+          :ingredientId="id"
+        ></LimitBar>
+      </v-col>
+    </v-row>
+
     <h1 class="text-h2">Intakes</h1>
     <IntakeForm></IntakeForm>
     <IntakeVisualizer></IntakeVisualizer>
