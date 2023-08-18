c84cd4517b29c6a4beef02e9c63723b0ecb89dd6
malte Liam Jakob <malteliam.jakob@rwu.de>
2023-08-18 09:22:58 +0200

    Implement calculation of current value and expiration of oldest intake

--- src/components/home/limitBar.vue
+++ src/components/home/limitBar.vue
@@ -1,9 +1,12 @@
 <script setup>
 import { computed } from 'vue';
-const props = defineProps([ 'ingredientInfo', 'currentValue', 'changes' ]);
+import { useIndexedStore } from '@/store/indexed';
+
+const indexedStore = useIndexedStore();
+const props = defineProps([ 'ingredientInfo', 'ingredientId' ]);
 
 const progress = computed(() => {
-  return (props.currentValue / props.ingredientInfo.limit) * 100;
+  return (currentIntake.value / props.ingredientInfo.limit) * 100;
 });
 
 const progressColor = computed(() => {
@@ -15,11 +18,59 @@ const progressColor = computed(() => {
     return 'error';
   }
 });
+
+const relevantIntakes = computed(() => {
+  let intakes = [];
+  for (let itemId in indexedStore.intakes) {
+    if (!indexedStore.pills[indexedStore.intakes[itemId].pill].contents.some((elem) => { return elem.ingredient == props.ingredientId })) {
+      continue;
+    }
+
+    let tmp = {
+      takenAt: indexedStore.intakes[itemId].takenAt
+    };
+
+    let relevantIngredient = indexedStore.pills[indexedStore.intakes[itemId].pill].contents.find((elem) => { return elem.ingredient == props.ingredientId });
+
+    tmp.amount = indexedStore.intakes[itemId].half ? relevantIngredient.amount / 2 : relevantIngredient.amount;
+
+    intakes.push(tmp);
+  }
+
+  intakes.sort((a, b)  => {
+    return b.takenAt - a.takenAt;
+  });
+
+  return intakes;
+});
+
+const currentIntake = computed(() => {
+  let sum = 0;
+  for (let intake of relevantIntakes.value) {
+    sum += intake.amount;
+  }
+
+  return sum;
+});
+
+const nextIntake = computed(() => {
+  if (relevantIntakes.value.length == 0) {
+    return { at: Date.now(), amount: props.ingredientInfo.limit }
+  }
+
+  let relevantIntake = relevantIntakes.value[relevantIntakes.value.length - 1];
+
+  return {
+    at: relevantIntake.takenAt + props.ingredientInfo.expires,
+    amount: relevantIntake.amount
+  };
+});
+
 </script>
 <template>
   <p class="text-h4">{{ props.ingredientInfo.name }}</p>
   <v-progress-linear :model-value="progress" :color="progressColor" height="25rem">
-    {{ props.currentValue }} of {{ props.ingredientInfo.limit }} mg
+    {{ currentIntake }} of {{ props.ingredientInfo.limit }} mg
   </v-progress-linear>
-  <p>In {{ props.changes.in }} you can take additional {{ props.changes.amount }} mg</p>
+  <p class="text-caption" v-if="relevantIntakes.length > 0">-{{ nextIntake.amount }} mg at {{ (new Date(nextIntake.at)).toLocaleString() }}</p>
 </template>
