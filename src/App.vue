bffc9a385590c3f1c329917e8db8f6eca44494aa
malte Liam Jakob <malteliam.jakob@rwu.de>
2024-11-17 14:37:50 +0100

    Implement Language Select

--- src/App.vue
+++ src/App.vue
@@ -1,11 +1,13 @@
 <script setup>
   import { onMounted, ref } from 'vue';
   import { useIndexedStore } from '@/store/indexed';
+  import { useI18n } from 'vue-i18n';
 
   import { handler as config } from '@/utils/ConfigHandler.js';
 
   import DecryptionModal from '@/components/decryptionModal.vue';
 
+  const i18n = useI18n();
   const indexedStore = useIndexedStore();
 
   const showDecryptionModal = ref(false);
@@ -20,6 +22,18 @@
     } else {
       await indexedStore.init();
       await indexedStore.markOrDeleteExpiredIntakes();
+    }
+
+    if (cfg.lang) {
+      i18n.locale.value = cfg.lang;
+    } else {
+      for (let lang of window.navigator.languages) {
+        let la = lang.split('-')[0];
+        if (i18n.availableLocales.includes(la)) {
+          i18n.locale.value = la;
+          break;
+        }
+      }
     }
   });
 
--- src/views/settings/language.vue
+++ src/views/settings/language.vue
@@ -1,7 +1,27 @@
 <script setup>
+import { ref } from 'vue';
+import { useConfigStore } from '@/store/config';
+import { useI18n } from 'vue-i18n';
+
+const configStore = useConfigStore();
+const { availableLocales, locale, t } = useI18n();
+
+const availableLocalesSelect = [];
+for (let locale of availableLocales) {
+  availableLocalesSelect.push({ value: locale, title: t('locale', 1, { locale: locale }) });
+}
+
+function saveLang(lang) {
+  configStore.config.lang = lang;
+}
 </script>
 <template>
   <v-container>
     <h1 class="text-h2">{{ $t('settings.language.title') }}</h1>
+    <v-select
+      :items="availableLocalesSelect"
+      v-model="locale"
+      @update:modelValue="saveLang"
+    ></v-select>
   </v-container>
 </template>
